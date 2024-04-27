6e4e63b17549713f8d985b6105cc0612fdcc4aa6
ibukunoluwanap <promisenap2001@gmail.com>
2024-04-27 19:56:20 +0300

    added payment card

--- app/(application)/payment/card/page.js
+++ app/(application)/payment/card/page.js
@@ -6,21 +6,23 @@ import BottomNavbar from "@/app/_components/navs/bottom_navbar";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Loader from "@/app/_components/loader";
+import visa from "@/public/images/visa.png";
+import master from "@/public/images/master.png";
+import Image from "next/image";
 
-export default function FlightCheckout() {
+export default function FlightCard() {
   const [isLoading, setIsLoading] = useState(false);
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [dob, setDob] = useState("");
   const [email, setEmail] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
   const [title, setTitle] = useState("Mr");
   const router = useRouter();
 
-  const onPayNow = async (e) => {
+  const onProceed = async (e) => {
     e.preventDefault();
     setIsLoading(true);
-    router.push("/payment/card");
+    router.push("/payment/flight_ticket");
   };
 
   return (
@@ -30,46 +32,29 @@ export default function FlightCheckout() {
       <div className="container my-5 px-md-5">
         <div className="row justify-content-center">
           <div className="col-12">
-            <h1 className="display-6">Checkout</h1>
+            <h1 className="display-6">Payment Options</h1>
           </div>
 
           <div className="col-md-6 mt-3">
             <div className="d-flex">
-              <button
-                onClick={() => setTitle("Mr")}
-                className={
-                  title === "Mr"
-                    ? "btn btn-lg btn-primary me-3 w-auto"
-                    : "border border-dark transparent rounded-4 px-4 me-3"
-                }
-              >
-                Mr.
-              </button>
+              <Image
+                src={visa}
+                width={57}
+                priority
+                alt="visa"
+                className="img-responsive me-3"
+              />
 
-              <button
-                onClick={() => setTitle("Mrs")}
-                className={
-                  title === "Mrs"
-                    ? "btn btn-lg btn-primary me-3 w-auto"
-                    : "border border-dark transparent rounded-4 px-4 me-3"
-                }
-              >
-                Mrs.
-              </button>
-
-              <button
-                onClick={() => setTitle("Miss")}
-                className={
-                  title === "Miss"
-                    ? "btn btn-lg btn-primary me-3 w-auto"
-                    : "border border-dark transparent rounded-4 px-4 me-3"
-                }
-              >
-                Miss.
-              </button>
+              <Image
+                src={master}
+                width={57}
+                priorite
+                alt="master"
+                className="img-responsive"
+              />
             </div>
 
-            <form onSubmit={onPayNow}>
+            <form onSubmit={onProceed}>
               <div className="form-floating my-4">
                 <input
                   type="text"
@@ -130,27 +115,12 @@ export default function FlightCheckout() {
                 </label>
               </div>
 
-              <div className="form-floating mb-4">
-                <input
-                  type="tel"
-                  required
-                  className="form-control cus-form-control"
-                  id="phoneNumber"
-                  placeholder="Phone Number"
-                  onChange={(e) => setPhoneNumber(e.target.value)}
-                />
-
-                <label className="form-label" htmlFor="phoneNumber">
-                  Phone Number
-                </label>
-              </div>
-
               <button
                 type="submit"
                 disabled={isLoading}
                 className="btn btn-lg btn-primary mt-5 w-100"
               >
-                {isLoading ? <Loader /> : "Pay Now"}
+                {isLoading ? <Loader /> : "Proceed"}
               </button>
             </form>
           </div>
