b27e58841a30b54f2d6ae3433ac6ad0fd608290a
Ibukunoluwa Naphtali <ibukunoluwanap@gmail.com>
2025-04-16 19:52:35 +0300

    upd

--- app/_components/flight/flight_search.js
+++ app/_components/flight/flight_search.js
@@ -379,10 +379,10 @@ const FlightSearch = () => {
                     className="form-select cus-form-control"
                     onChange={(e) => setCabinClass(e.target.value)}
                   >
-                    <option value="ECONOMY">Economy</option>
-                    <option value="PREMIUM_ECONOMY">Premium economy</option>
-                    <option value="BUSINESS">Business</option>
-                    <option value="FIRST">First</option>
+                    <option value="economy">Economy</option>
+                    <option value="premium_economy">Premium economy</option>
+                    <option value="business">Business</option>
+                    <option value="first">First</option>
                   </select>
                 </div>
 
